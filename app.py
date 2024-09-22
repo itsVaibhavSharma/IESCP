df6db0382a53dab6ad8f4b8aea60b3f88be8d692
itsVaibhavSharma <thevaibhavsharmaofficial@gmail.com>
2024-09-22 18:57:02 +0530

    integrating redis

--- app.py
+++ app.py
@@ -2,8 +2,27 @@ from flask import Flask
 from config import Config
 from models import db, Admin
 from routes import main_bp
+import base64
+
+from flask import Flask, session
+from flask_session import Session
+import redis
 
 app = Flask(__name__)
+
+# Redis configuration
+app.config['SESSION_TYPE'] = 'redis'
+app.config['SESSION_REDIS'] = redis.StrictRedis(
+    host='redis-15695.c232.us-east-1-2.ec2.redns.redis-cloud.com',
+    port=15695,
+    # password='<your_redis_password>'  # Replace with your Redis password if one is set
+)
+app.config['SESSION_PERMANENT'] = False
+app.config['PERMANENT_SESSION_LIFETIME'] = 3600  # Set session lifetime (optional)
+
+# Initialize Flask-Session
+Session(app)
+
 app.config.from_object(Config)
 
 db.init_app(app)
@@ -17,6 +36,10 @@ with app.app_context():
         db.session.add(admin)
         db.session.commit()
     
+
+@app.template_filter('b64encode')
+def b64encode(data):
+    return base64.b64encode(data).decode('utf-8')
 
 app.register_blueprint(main_bp)
 
--- config.py
+++ config.py
@@ -7,8 +7,15 @@
 # Using postgres for vercel deployment
 import os
 import secrets
+import redis
 
 class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY', secrets.token_hex(16))
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL', 'postgresql://default:lmeuKtvbA9r0@ep-morning-voice-a4ds1ucg.us-east-1.aws.neon.tech:5432/verceldb?sslmode=require')
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL', 'your_postgres_url')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
+    SESSION_TYPE = 'redis'
+    SESSION_REDIS = redis.StrictRedis(
+        host='redis-15695.c232.us-east-1-2.ec2.redns.redis-cloud.com',
+        port=15695,
+        password='<your_redis_password>'
+    )
