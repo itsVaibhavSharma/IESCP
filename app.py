293843b64f038ad5765f0913359b17efbd2225c9
itsVaibhavSharma <thevaibhavsharmaofficial@gmail.com>
2024-09-22 17:29:21 +0530

    Changes for Deployment

--- app.py
+++ app.py
@@ -7,6 +7,7 @@ app = Flask(__name__)
 app.config.from_object(Config)
 
 db.init_app(app)
+
 with app.app_context():
     db.create_all()
 
--- config.py
+++ config.py
@@ -1,5 +1,14 @@
 
+# class Config:
+#     SECRET_KEY = "MySecKey"
+#     SQLALCHEMY_DATABASE_URI="sqlite:///v1.db"
+#     SQLALCHEMY_TRACK_MODIFICATION = False
+
+# Using postgres for vercel deployment
+import os
+import secrets
+
 class Config:
-    SECRET_KEY = "MySecKey"
-    SQLALCHEMY_DATABASE_URI="sqlite:///v1.db"
-    SQLALCHEMY_TRACK_MODIFICATION = False
+    SECRET_KEY = os.environ.get('SECRET_KEY', secrets.token_hex(16))
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL', 'postgresql://default:lmeuKtvbA9r0@ep-morning-voice-a4ds1ucg.us-east-1.aws.neon.tech:5432/verceldb?sslmode=require')
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
